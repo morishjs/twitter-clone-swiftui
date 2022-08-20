c91b17b216a9a19c0c0f4726416acc8718eccc49
Junsuk Park <park64kr63@gmail.com>
2022-08-20 15:22:16 +0900

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Park Junsuk on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
